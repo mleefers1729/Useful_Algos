6bbf3ef48c1290eddb6471d064be93a30b397bcc
Mitchell Leefers <mleefers@roeslein.com>
2022-11-29 22:48:20 -0600

    Added comments to the SnowflakeSearch Code to clarity

--- SnowflakeSearch.c
+++ SnowflakeSearch.c
@@ -1,19 +1,26 @@
+/*This solution implements a hash table of linked lists for the search.
+For an input of 100,000 snowflakes, this program should take < 2seconds to complete the search.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #define MAX_LINE_LENGTH 1000
 #define SIZE 100000
 
+//using typedef and struct to define a structure for linked list nodes that we will use to create our hash table
 typedef struct flake_node {
 	int flake[6];
-	struct flake_node *next;
+	struct flake_node* next;
 }flake_node;
 
+//Hashing function for the snowflakes. Add the elements then mod by by the max number of snowflakes
 int code(int flake[])
 {
 	return (flake[0] + flake[1] + flake[2] + flake[3] + flake[4] + flake[5]) % SIZE;
 }
 
+//checking for right to left match
 int moving_right(int sf1[], int sf2[], int start)
 {
 	for (int i = 0; i < 6; ++i)
@@ -24,6 +31,7 @@ int moving_right(int sf1[], int sf2[], int start)
 	return 1;
 }
 
+//checking for left to right match
 int moving_left(int sf1[], int sf2[], int start)
 {
 	for (int i = 0; i < 6; ++i)
@@ -37,7 +45,7 @@ int moving_left(int sf1[], int sf2[], int start)
 	return 1;
 }
 
-
+//moving through a list of snowflakes and checking them
 int are_identical(int sf1[], int sf2[])
 {
 	int start;
@@ -55,9 +63,11 @@ int are_identical(int sf1[], int sf2[])
 	return 0;
 }
 
-void find_identical(flake_node *flakes[])
+//The function that will return the result and call all other functions needed to find the identical snowflakes. We are initializing and using the linked
+//list nodes with the 'are_identical' funtion defined above.
+void find_identical(flake_node* flakes[])
 {
-	flake_node *node1, *node2;
+	flake_node* node1, * node2;
 	for (int i = 0; i < SIZE; ++i)
 	{
 		node1 = flakes[i];
@@ -81,40 +91,46 @@ void find_identical(flake_node *flakes[])
 	return;
 }
 
+//We are reading the input from a text file in this function. Any kind of input could be used but I found it quick to use Python to create a bunch of lists of 
+//random integers and output a text file. Python code for that can be found in the repo and is called "SnowflakeInput.py".
 int main(void)
 {
+	//Starting by printing a little bit of a heading to make the output look nicer. It matters okay??
 	printf("%c", 10);
-	printf("----------Algo Testing Number 2 - Snowflake Search Algo-----------%c", 10);
+	printf("----------Snowflake Search Algo-----------%c", 10);
 	printf("%c", 10);
-	
-	FILE * textfile;
+
+	//Defining variables
+	FILE* textfile;
 	char line[MAX_LINE_LENGTH];
-	static flake_node *flakes[SIZE] = {NULL};
-	flake_node *snow;
+	static flake_node* flakes[SIZE] = { NULL };
+	flake_node* snow;
 	int tmpFlake[6];
 	int flakeCode;
-	//int lineCount = 0;
 
-	textfile = fopen("SnowflakeInput.txt","r");
-	
+	//opening the text file and looping through each line using fgets to get the list of integers.
+	textfile = fopen("SnowflakeInput.txt", "r");
+
+	//This loops through each line of the file
 	while (fgets(line, MAX_LINE_LENGTH, textfile))
 	{
 		char* ptr = strtok(line, ",");
 		int idx = 0;
 		snow = malloc(sizeof(flake_node));
-		//printf("%c\n", line);
 		if (snow == NULL)
 		{
 			fprintf(stderr, "malloc error\n");
 			exit(1);
 		}
+		//This actually loops through the text on each line. We loop through and get all the integers and put them in an array called tmpFlake. 
 		while (ptr != NULL)
 		{
 			tmpFlake[idx] = atoi(ptr);
 			ptr = strtok(NULL, ",");
 			++idx;
 		}
-		
+		//Getting rid of unwanted 0s caued by the strtok function used above. Then getting the hashcode for the flake and building the linked list
+		//and inserting it into our hashtable at the the index calculated for the hashcode.
 		for (int r = 0; r < 6; r++)
 		{
 			if (tmpFlake[r] != 0)
@@ -125,10 +141,11 @@ int main(void)
 		flakes[flakeCode] = snow;
 	}
 
+	//Closing the file and finally calling the find_identical function on the hash table that we have created.
 	fclose(textfile);
 	printf("\nInput File Closed\nFinding Identical Flakes...\n");
 	find_identical(flakes);
 
-	printf("%c", 10);
+	printf("\n");
 	return 0;
 }
