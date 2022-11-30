60921a8602396f00fd91e36033b375c60d9e8356
Mitchell Leefers <55854922+mleefers1729@users.noreply.github.com>
2022-11-29 22:55:16 -0600

    Update SnowflakeInput.py
    Added comment for info

--- SnowflakeInput.py
+++ SnowflakeInput.py
@@ -1,7 +1,11 @@
-import random
-import string
+'''
+This is used to generate random snowflakes for the snowflake search algo in the repo. It outputs a text file with one snowflake per line of the file.
+You will need to update the code for the algo to match your filepath here as well. This should take less than 1 sec to generate a 100,000 line file.
+'''
 
-numFlakes = 50000
+import random
+
+numFlakes = 100000
 numRods = 6
 flakes = []
 for i in range(numFlakes):
@@ -10,7 +14,7 @@ for i in range(numFlakes):
 		flake.append(random.randint(0,10000000))
 	flakes.append(flake)
 
-with open('C:/Users/mitchell leefers/Documents/MyFiles/SnowflakeInput.txt','w') as f:
+with open('YourFilePathHere/SnowflakeInput.txt','w') as f:
 	for flk in flakes:
 		line = ''
 		for val in flk:
